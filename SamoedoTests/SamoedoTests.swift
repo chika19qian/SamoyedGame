337e9485761e39fec41dba606bf11f87016f001c
chika19qian <chika5747@g.ecc.u-tokyo.ac.jp>
2025-02-06 13:49:12 +0900

    Initial Commit

---
+++ SamoedoTests/SamoedoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SamoedoTests.swift
+//  SamoedoTests
+//
+//  Created by chika on 2025/2/6.
+//
+
+import Testing
+@testable import Samoedo
+
+struct SamoedoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
